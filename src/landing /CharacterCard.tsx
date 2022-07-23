43ad612e50cbe19c55e16a074596dcde76d1d2f8
Yazmín Trujillo López <97549396+Yazmin-Trujillo@users.noreply.github.com>
2022-07-23 15:34:03 -0500

    Sass (#1)
    * se agregó sass
    
    * media query para 1024px

--- src/landing /CharacterCard.tsx
+++ src/landing /CharacterCard.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import './CharacterCard.css';
+import './CharacterCard.scss';
 
 function CharacterCard() {
     return (
-        <article className='tarjeta'>
-            <div className='perfil'>
-                <img src='image' className="imagenPerfil" alt="ImagenDePerfil" />
+        <article className='characterCard'>
+
+            <div className='profileImage'>
+                <img src='image' className="profileImage" alt="profileImage" />
             </div>
-            <section className='informaciónTarjeta'>
-                <h1 className='nombrePersonaje'>Harry Potter</h1>
-                <div className='contenidoTarjeta'>
-                    <div className='estados'>
-                        <h4>VIVO</h4>
-                        <h4>ESTUDIANTE</h4>
+
+            <section className='cardInformation'>
+
+                <div className='statusInformation'>
+                    <div className='statuses'>
+                        <h4>VIVO / ESTUDIANTE</h4>
                     </div>
-                    <div>
-                        <img src='./images/Rectangle.png' className="iconoRectangulo" alt="iconoRectangulo" />
+                    <div className="iconoRectangulo" >
+                        <img src='./images/Rectangle.png' alt="iconoRectangulo" />
                     </div>
                 </div>
 
+                <div className='characterInformation'>
+                    <div className='charactersName'>
+                        <h1 className='name'>Harry Potter</h1>
+                    </div>
+                    <div className='profile'>
+                        <h5>Cumpleaños:</h5>
+                        <h5>Género:</h5>
+                        <h5>Color de ojos:</h5>
+                        <h5>Color de pelo:</h5>
+                    </div>
+                </div>
                 {/* <h1 className='nombrePersonaje'>name del personaje</h1>
                 <h4>alive si es true retornar vivo sino finado</h4>
                 <h4>hogwartsStudent:true retornar estudiante, hogwartsStaff:true retornar staff</h4> */}
@@ -29,3 +41,15 @@ function CharacterCard() {
 }
 
 export default CharacterCard;
+
+{/* <h1 className='charactersName'>Harry Potter</h1>
+                <div className='cardContent'>
+                    <div className='statuses'>
+                        <h4>VIVO</h4>
+                        <h4>ESTUDIANTE</h4>
+                    </div>
+                    <div>
+                        <img src='./images/Rectangle.png' className="iconoRectangulo" alt="iconoRectangulo" />
+                    </div>
+                </div> */}
+
--- src/landing /Gallery.tsx
+++ src/landing /Gallery.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
-import './Gallery.css';
+import './Gallery.scss';
 
 function Gallery() {
     return (
         <div className="gallery">
-            <img src='./images/Harry_Potter.png' className="titulo" alt="HarryPotter" />
-            <h4 className='subtitulo'>Selecciona tu filtro</h4>
-            <div className='contenedorBotonesCategoria'>
-                <button className='categoria'>ESTUDIANTES</button>
-                <button className='categoria'>STAFF</button>
+            <img src='./images/Harry_Potter.png' className="namePage" alt="HarryPotter" />
+            <h4 className='instructions'>Selecciona tu filtro</h4>
+            <div className='categoryButtonContainer'>
+                <button className='category'>ESTUDIANTES</button>
+                <button className='category'>STAFF</button>
             </div>
-            <main className='contenido'>
+            <main className='contents'>
                 <CharacterCard />
                 <CharacterCard />
             </main>
