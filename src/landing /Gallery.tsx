5817a6b73c93e76919a636bea3b74b6884a8c936
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-07-24 18:56:15 -0500

    funcionalidad para mostrar estudiantes  o staff

--- src/landing /Gallery.tsx
+++ src/landing /Gallery.tsx
@@ -5,6 +5,9 @@ import './Gallery.scss';
 
 function Gallery() {
     const [characters, setCharacters] = useState<Character[]>([]);
+    // const [students, setStudents] = useState<Character[]>([])
+    const [studentsToggle, setStudentsToggle] = useState(true);
+    const [staffToggle, setStaffToggle] = useState(true)
 
     useEffect(() => {
         readCharacters()
@@ -13,7 +16,7 @@ function Gallery() {
     async function readCharacters() {
         const characters = await dataService.getCharacters();
 
-        // const orderedProducts = products.sort((a, b) => {
+        // const orderedCharacters = characters.sort((a, b) => {
         //     if (a.name.toLowerCase() < b.name.toLowerCase()) {
         //         return -1;
         //     }
@@ -22,21 +25,57 @@ function Gallery() {
         //     }
         //     return 0;
         // });
+        // setCharacters(orderedCharacters)
+        // console.log(characters);
         setCharacters(characters)
         console.log(characters);
     }
+
+    async function studentsFilter() {
+        setStaffToggle(true)
+        setStudentsToggle(!studentsToggle)
+        console.log('students', studentsToggle)
+        if (studentsToggle === true) {
+            const characters = await dataService.getCharacters();
+            const students = characters.filter(character => character.hogwartsStudent === true)
+
+            setCharacters(students)
+            console.log(students)
+        } else {
+            readCharacters()
+        }
+    }
+
+    async function staffFilter() {
+        setStudentsToggle(true)
+        setStaffToggle(!staffToggle)
+
+        if (staffToggle === true) {
+            const characters = await dataService.getCharacters();
+
+            const staff = characters.filter(character => character.hogwartsStaff === true)
+
+            setCharacters(staff)
+            console.log(staff)
+        } else {
+            readCharacters()
+        }
+
+    }
+
+
 
     return (
         <div className="gallery">
             <img src='./images/Harry_Potter.png' className="namePage" alt="HarryPotter" />
             <h4 className='instructions'>Selecciona tu filtro</h4>
             <div className='categoryButtonContainer'>
-                <button className='category'>ESTUDIANTES</button>
-                <button className='category'>STAFF</button>
+                <button className='category' onClick={studentsFilter} >ESTUDIANTES</button>
+                <button className='category' onClick={staffFilter}>STAFF</button>
             </div>
             <main className='contents'>
-            {characters.map((character) => {
-                    return <CharacterCard key={character.name} character={character}/>
+                {characters.map((character) => {
+                    return <CharacterCard key={character.name} character={character} />
                 })}
             </main>
 
